cd83589f8f59bfd012b19fa8e6e9fcf77b9dae22
Julien Dessaux <judessaux@gmail.com>
2009-01-19 19:24:24 +0100

    Propagated the changes in data structures from the server side to the client side.

--- client/include/ricart_rpc.h
+++ client/include/ricart_rpc.h
@@ -1,6 +1,12 @@
+/** 
+ * @file ricart_rpc.h
+ * @brief The RPC tools header
+ * @author Julien Dessaux & Jan Villeminot
+ * @date 2009-01-19
+ */
 
-#ifndef ricart_h
-#define ricart_h
+#ifndef _RICART_H_
+#define _RICART_H_
 
 #include <rpc/types.h>
 #include <rpc/xdr.h>
@@ -11,17 +17,34 @@
 #define NUMERO_PROG 0x7044400
 #define NUMERO_VERSION 1
 #define REGISTER_NB 1
+
 #define MAX_NAME_SIZE 8
 
-/*
- * registerrpc(n°Prog, n°Version, n°proc, nom proc, nom param, nom return)
- *
- */
-struct couple{
-   int port;
-   char nom[MAX_NAME_SIZE+1];
-};
-int xdr_couple();
-char*register_on_server();
+//! @brief The data structure for a request
+typedef struct request{
+    //! @brief A tabular for the client's name
+    char name[MAX_NAME_SIZE + 1];
+    //! @brief The UDP port the client is binded on
+    int port;
+} request_t;
 
+//! @brief The function that will handle a request data structure
+//! @param xdrp The XDR pointer
+//! @param p A pointer on the data structure to handle
+int xdr_request(XDR *xdrp, request_t *p);
+
+
+//! @brief The data structure for a response
+typedef struct response{
+    //! @brief The number of clients that are ready, or 0 if an error occured
+    int nb_clients;
+    //! @brief The port provided by the client, or an error code (100: too many clients, 200: port number error)
+    int port;
+} response_t;
+
+//! @brief The function that will handle a response data structure
+//! @param xdrp The XDR pointer
+//! @param p A pointer on the data structure to handle
+int xdr_response(XDR *xdrp, response_t *p);
 #endif
+
--- client/src/ricart_rpc.c
+++ client/src/ricart_rpc.c
@@ -1,6 +1,18 @@
+/** 
+ * @file ricart_rpc.c
+ * @brief The RPC tools implementation
+ * @author Julien Dessaux & Jan Villeminot
+ * @date 2009-01-19
+ */
+
 #include "ricart_rpc.h"
 
-int xdr_couple(XDR *xdrp, struct couple *p){
-   return(xdr_int(xdrp,&p->port) && xdr_vector(xdrp, p->nom,MAX_NAME_SIZE,
-   					sizeof(char),(xdrproc_t) xdr_char));
+int xdr_request(XDR *xdrp, request_t *p){
+    return (xdr_int(xdrp, &p->port) &&
+	    xdr_vector(xdrp, p->name, MAX_NAME_SIZE, sizeof(char), (xdrproc_t) xdr_char));
 }
+
+int xdr_response(XDR *xdrp, response_t *p){
+    return (xdr_int(xdrp, &p->nb_clients) && xdr_int(xdrp, &p->port));
+}
+
