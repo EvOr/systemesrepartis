2363dcf093e92934a520039598a7b6fa88cd1f68
EvOr <villemin@poste.isima.fr>
2009-02-06 02:35:40 +0000

    Modification du script client, modification diagramme, ajout cas utilisation au rapport

--- client.sh
+++ client.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 xterm -T Client1 -e client/client Client1 23456 &
+sleep 1
 xterm -T Client2 -e client/client Client2 23457 &
+sleep 1
 xterm -T Client3 -e client/client Client3 23458 &
